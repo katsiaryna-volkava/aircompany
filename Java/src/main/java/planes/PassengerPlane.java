6e33776eb0cfb454ffc3dd8953adb77d81182b24
Katsiaryna Volkava <Katsiaryna_Volkava@epam.com>
2019-08-06 15:36:09 +0300

    changing equals method

--- Java/src/main/java/planes/PassengerPlane.java
+++ Java/src/main/java/planes/PassengerPlane.java
@@ -21,18 +21,18 @@ public class PassengerPlane extends Plane {
                 ", passengersCapacity=" + passengersCapacity + '}');
     }
 
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PassengerPlane)) return false;
-        if (!super.equals(o)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PassengerPlane)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
         PassengerPlane plane = (PassengerPlane) o;
         return passengersCapacity == plane.passengersCapacity;
     }
